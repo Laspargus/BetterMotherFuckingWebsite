575ecdf2adae8c75b4302302a988744646cc5612
Laspargus <maxime.speroni@gmail.com>
2018-12-28 15:59:16 +0100

    Ajout du bouton

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,9 @@ if (confirm ('Hey le site xxxvidsxxx est trop bien. Veux tu le visiter ?'))
     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "C'est cadeau")  
 };
 }
+
+function modePsy(){
+    document.body.style.backgroundColor = "magenta";
+    var paragraphe = document.getElementsByTagName("p");
+    paragraphe.style.setProperty('color','blue');
+}
